d5a4d463f3dd0c931a8a09c64f52bcb875fae9d3
robsonfm <nosborffm@gmail.com>
2016-06-19 11:59:47 -0300

    Upando o projeto novamente

--- JAXWSClient/src/org/engsoft/webservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.engsoft.org/")
-package org.engsoft.webservice;
--- JAXWSServer/src/org/engsoft/webservice/LivrariaWSPublisher.java
+++
@@ -1,8 +0,0 @@
-package org.engsoft.webservice;  
-import javax.xml.ws.Endpoint;  
-
-public class LivrariaWSPublisher {
-	public static void main(String[] args) {  
-		Endpoint.publish("http://localhost:8080/WS/ILivraria", new Livraria());  
-	}
-}
